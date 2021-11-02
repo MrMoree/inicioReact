1e21c72b2fa410720b485e7e75d3cff4e139a487
Moree <marianomorellato@gmail.com>
2021-11-02 20:32:01 -0300

    Your Message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import NavBar from './components/navegacion/NavBar';
+import Index from './components/paginas/Index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <Router>
+    <NavBar />
+    </Router>
+    
     </div>
-  );
-}
+    );
+  }
+  
+  export default App;
 
-export default App;
+
+  // https://mdbootstrap.com/docs/standard/navigation/navbar/   NAVBAR USADA
